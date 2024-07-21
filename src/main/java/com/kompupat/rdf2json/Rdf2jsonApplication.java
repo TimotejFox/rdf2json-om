82c43e7f9bcb3f808fc931fefb49ea21924bd81e
TimotejFox <patrik.kompus@gmail.com>
2024-07-21 11:29:56 +0200

    init

---
+++ src/main/java/com/kompupat/rdf2json/Rdf2jsonApplication.java
@@ -0,0 +1,13 @@
+package com.kompupat.rdf2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdf2jsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rdf2jsonApplication.class, args);
+    }
+
+}
